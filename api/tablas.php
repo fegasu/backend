f430fcfd9d3761db3ebbe41217dfdd66bc210ed6
Jose Fegasu <fegasu@misena.edu.co>
2022-01-29 11:31:21 -0500

    .

---
+++ api/tablas.php
@@ -0,0 +1,21 @@
+<?php
+  $peticion=$_GET["peticion"];
+  $opcion=$_GET["opcion"];
+  $parte=$_GET["parte"];
+ if($peticion == "carrito")
+    echo $ser->mostrarCarrito($opcion);
+elseif($peticion == "carritoi")
+    echo $ser->insertarCarrito($opcion,$parte);
+elseif($peticion == "categoria")
+    echo $ser->mostrarCategoria($opcion);
+elseif($peticion == "vpedido")
+    echo $ser->mostrarPedido($opcion);
+elseif($peticion == "plato")
+    echo $ser->mostrarPlatos($opcion);
+else{
+  echo "HTTP/1.1 404 Servicio no encontrado";
+  header("HTTP/1.1 404 Servicio no encontrado");
+}
+
+?>
+
